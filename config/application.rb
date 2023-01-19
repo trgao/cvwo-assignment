a52f68d354795363ea5e58ba6448857dcecd56e7
trgao <112098096+trgao@users.noreply.github.com>
2023-01-19 11:20:13 +0800

    Fix

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module ReactApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.assets.enabled = false
   end
 end
